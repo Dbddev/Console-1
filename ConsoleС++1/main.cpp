9bdd78d51f39b575f8975688ded80baa6f350fde
Zmitrovich Egor <egorpomidor2077@gmail.com>
2025-04-15 23:17:46 +0300

    full change

--- ConsoleС++1/main.cpp
+++ ConsoleС++1/main.cpp
@@ -4,24 +4,24 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "RU");
-	float n, b, l;
-	cout << "Enter 2 numbers: " << endl;
-	cin >> n;
-	cin >> b;
-	cout << "Choose operation: " << endl << "Press 1 to divide " << endl << "Press 2 to multiply " << endl << "Press 3 to sum up" << endl << "Press 4 to subtract" << endl;
-	cin >> l;
-	if (l == 1) {
+	float n;
+	float b;
+	char l;
+	cout << "Enter operation: " << endl;
+	cin >> n >> l >> b;
+
+	if (l == '/') {
 		float a1 = n / b;
 		cout << "Answer: " << a1; }
-	if (l == 2) {
+	else if (l == '*') {
 		float  a2 = n * b;
 		cout << "Answer: " << a2;
 	}
-	if (l == 3) {
+	else if (l == '+') {
 		float a3 = n + b;
 		cout << "Answer: " << a3;
 	}
-	if (l == 4) {
+	else if (l == '-') {
 		float a4 = n - b;
 		cout << "Answer: " << a4;
 	}
